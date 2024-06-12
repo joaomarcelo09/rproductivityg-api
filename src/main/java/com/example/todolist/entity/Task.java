9d86b8f2b5b21ebd7eaa73532c2ef56b38cd7fca
Joao Gomes <jg0112360@gmail.com>
2024-06-11 22:52:01 -0300

    feat: initial commit

---
+++ src/main/java/com/example/todolist/entity/Task.java
@@ -0,0 +1,64 @@
+package com.example.todolist.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tasks")
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String title;
+    private String description;
+    private int priority;
+    private Boolean completed;
+    private String date_limit;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
+    public Boolean getCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(Boolean completed) {
+        this.completed = completed;
+    }
+
+    public String getDate_limit() {
+        return date_limit;
+    }
+
+    public void setDate_limit(String date_limit) {
+        this.date_limit = date_limit;
+    }
+}
