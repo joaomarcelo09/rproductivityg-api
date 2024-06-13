853876656fd76850fd72e14460f4643e883f1fe5
Joao Gomes <jg0112360@gmail.com>
2024-06-13 15:09:45 -0300

    feat: custom user details service

---
+++ src/main/java/com/example/todolist/infra/security/CustomUserDetailsService.java
@@ -0,0 +1,26 @@
+package com.example.todolist.infra.security;
+
+import com.example.todolist.entity.User;
+import com.example.todolist.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+
+@Component
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository repository;
+
+    public CustomUserDetailsService(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = this.repository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), new ArrayList<>());
+    }
+}
