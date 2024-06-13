09cf28d469c186de0cf5565819bc9336ff656735
Joao Gomes <jg0112360@gmail.com>
2024-06-13 15:42:59 -0300

    feat: auth dto

---
+++ src/main/java/com/example/todolist/dto/LoginRequestDto.java
@@ -0,0 +1,4 @@
+package com.example.todolist.dto;
+
+public record LoginRequestDto(String email, String password) {
+}
---
+++ src/main/java/com/example/todolist/dto/RegisterRequestDto.java
@@ -0,0 +1,4 @@
+package com.example.todolist.dto;
+
+public record RegisterRequestDto(String name, String email, String password) {
+}
