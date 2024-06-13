1c1099c5079fcc59c174c925fa8a1911912dd18e
Joao Gomes <jg0112360@gmail.com>
2024-06-13 15:23:53 -0300

    feat: Token Service

---
+++ src/main/java/com/example/todolist/infra/security/TokenService.java
@@ -0,0 +1,50 @@
+package com.example.todolist.infra.security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.example.todolist.entity.User;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
+public class TokenService {
+
+    @Value("123456")
+    private String secret;
+    public String generateToken(User user) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+
+            return JWT.create()
+                    .withIssuer("to-do-list-java")
+                    .withSubject(user.getEmail())
+                    .withExpiresAt(this.generateExpirationDate())
+                    .sign(algorithm);
+        } catch (Exception e) {
+            throw new RuntimeException("Error generating token", e);
+        }
+    }
+
+    private Instant generateExpirationDate() {
+        return LocalDateTime.now().plusHours(24).toInstant(ZoneOffset.ofHours(3));
+    }
+
+    public String validateToken(String token) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+
+            return JWT.require(algorithm)
+                    .withIssuer("to-do-list-java")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        } catch (JWTVerificationException e) {
+            return e.getMessage();
+        }
+    }
+}
