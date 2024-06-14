19504f51026bc6ab031d8a985ccc541dc16fbae4
Joao Gomes <jg0112360@gmail.com>
2024-06-14 16:10:07 -0300

    fix: user relation

--- src/main/java/com/example/todolist/entity/User.java
+++ src/main/java/com/example/todolist/entity/User.java
@@ -1,10 +1,14 @@
 package com.example.todolist.entity;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "users")
@@ -19,4 +23,8 @@ public class User {
     private String name;
     private String email;
     private String password;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
+    private Set<Task> task = new HashSet<>();
 }
