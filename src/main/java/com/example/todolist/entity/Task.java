7c8b715f036a587bcc26a86cb3e79b05db4aabad
Joao Gomes <jg0112360@gmail.com>
2024-06-14 16:09:10 -0300

    fix: task relation

--- src/main/java/com/example/todolist/entity/Task.java
+++ src/main/java/com/example/todolist/entity/Task.java
@@ -1,9 +1,18 @@
 package com.example.todolist.entity;
 
+
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "tasks")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Task {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -14,51 +23,7 @@ public class Task {
     private Boolean completed;
     private String date_limit;
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public int getPriority() {
-        return priority;
-    }
-
-    public void setPriority(int priority) {
-        this.priority = priority;
-    }
-
-    public Boolean getCompleted() {
-        return completed;
-    }
-
-    public void setCompleted(Boolean completed) {
-        this.completed = completed;
-    }
-
-    public String getDate_limit() {
-        return date_limit;
-    }
-
-    public void setDate_limit(String date_limit) {
-        this.date_limit = date_limit;
-    }
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
 }
