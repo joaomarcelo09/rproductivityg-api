35bf968fb49bba2939ceaf38c586e85d8dec939f
Joao Gomes <jg0112360@gmail.com>
2024-09-25 16:12:01 -0300

    feat(error): new global exception handler

---
+++ src/main/java/com/example/todolist/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.example.todolist.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(UserMismatchException.class)
+    public ResponseEntity<String> handleUserMismatchException(UserMismatchException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleGenericException(Exception e) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+    }
+}
