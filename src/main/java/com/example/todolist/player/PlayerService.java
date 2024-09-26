49f0b86df87f8033a6ba920fe83522d22e6f422d
Joao Gomes <jg0112360@gmail.com>
2024-09-26 14:21:00 -0300

    feat(player): new increase level service

--- src/main/java/com/example/todolist/player/PlayerService.java
+++ src/main/java/com/example/todolist/player/PlayerService.java
@@ -1,13 +1,24 @@
 package com.example.todolist.player;
 
 import com.example.todolist.classes.Classes;
+import com.example.todolist.user.User;
+import com.example.todolist.user.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class PlayerService {
     private final PlayerRepository playerRepository;
-    public PlayerService(PlayerRepository playerRepository) {
+    private final UserService userService;
+
+    @Autowired
+    public PlayerService(PlayerRepository playerRepository, UserService userService) {
         this.playerRepository = playerRepository;
+        this.userService = userService;
+
+
     }
 
     public Player save(Classes playerClass) {
@@ -23,4 +34,30 @@ public class PlayerService {
 
         return newPlayer;
     }
+
+    public Player increaseLevel(Long userID) {
+        Optional<User> userOpt = userService.findById(userID);
+
+        if (userOpt.isEmpty()) {
+            throw new RuntimeException("User not found");
+        }
+
+        User user = userOpt.get();
+        Player player = user.getPlayer();
+        int playerXpIncreased = player.getCurrent_experience() + 12;
+
+        if (playerXpIncreased >= player.getExperience_to_up()) {
+            player.setLevel(player.getLevel() + 1);
+            player.setCurrent_experience(0);
+            player.setExperience_to_up(player.getExperience_to_up() + player.getIncreaser());
+        } else {
+            player.setCurrent_experience(playerXpIncreased);
+        }
+
+        playerRepository.save(player);
+        userService.update(user);
+
+        return player;
+    }
+
 }
