dc322488265b2a3c8e31a801db9c8adebe767041
Joao Gomes <jg0112360@gmail.com>
2024-09-27 12:02:37 -0300

    feat(error): new default message in exceptions

--- src/main/java/com/example/todolist/exceptions/TaskNotFoundException.java
+++ src/main/java/com/example/todolist/exceptions/TaskNotFoundException.java
@@ -1,6 +1,9 @@
 package com.example.todolist.exceptions;
 
 public class TaskNotFoundException extends RuntimeException {
+    public TaskNotFoundException() {
+        super("Task not found.");
+    }
     public TaskNotFoundException(String message) {
         super(message);
     }
--- src/main/java/com/example/todolist/exceptions/UserNotFound.java
+++ src/main/java/com/example/todolist/exceptions/UserNotFound.java
@@ -1,6 +1,7 @@
 package com.example.todolist.exceptions;
 
 public class UserNotFound extends RuntimeException {
+    public UserNotFound() {super("User not found");}
     public UserNotFound(String message) {
         super(message);
     }
